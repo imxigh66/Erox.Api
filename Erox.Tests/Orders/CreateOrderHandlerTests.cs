15c9c900874a1112eac5bfacc651ec01f724a24b
imxigh66 <vladakasapcuk@gmail.com>
2025-11-02 21:47:00 +0200

    unit-test-orders

---
+++ Erox.Tests/Orders/CreateOrderHandlerTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Erox.Application.Orders.CommandHandler;
+using Erox.Application.Orders.Command;
+using Erox.DataAccess;
+using Erox.Domain.Aggregates.OrderAggregate;
+
+public class CreateOrderHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldCreateOrder_WhenValidRequest()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "OrderTestDb")
+            .Options;
+
+        await using var context = new DataContext(options);
+        var handler = new CreateOrderHandler(context);
+
+        var command = new CreateOrder
+        {
+            UserId = Guid.NewGuid(),
+            PaymentMethod = "Card",
+            ShippingMethod = "Courier",
+            Address = "Chisinau, str. Stefan cel Mare 10",
+            Status = "Pending",
+            Sum = 1999,
+            Items = new[]
+            {
+                 new CreateOrderItem
+                 {
+                     ProductId = Guid.NewGuid(),
+                     SizeId = Guid.NewGuid(),
+                     Quantity = 2
+                 }
+             }
+
+        };
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsError.Should().BeFalse();
+        var order = await context.Orders.Include(o => o.Items).FirstOrDefaultAsync();
+        order.Should().NotBeNull();
+        order!.UserId.Should().Be(command.UserId);
+        order.Items.Should().HaveCount(1);
+        order.Sum.Should().Be(1999);
+    }
+}
---
+++ Erox.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erox.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
